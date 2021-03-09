cc1107c02937d7d11a584b7c8a8ff387f5c4e72a
Olaf Łuczak <olafhd4@gmail.com>
2021-03-09 10:05:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import discord
+import os
+from discord.ext import commands
+from keep_alive import keep_alive
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if msg.startswith('Osa zdążysz?'):
+    await message.channel.send('Zdążę? Zdążę')
+
+  if msg.startswith('Osa jak tam giełda?'):
+    await message.channel.send('Trzeba inwestować w odpowiednich momentach a potem sprzedawać. Zawsze po lekcji i w trakcie lekcji jadę na giełdę.')
+
+  if msg.startswith('Osa jesteś strasznie zimny'):
+    await message.channel.send('Hej wszystkim, jestem osa, kocham was ale nie jestem KURWA MAĆ ZIMNY!')
+
+  if msg.startswith('Osa tak tak tak'):
+    await message.channel.send('TAK TAK TAK TAK TAK')
+
+  if msg.startswith('Osa ile kobiet wyruchałeś?'):
+    await message.channel.send('A ile zjadłeś kromek chleba w twoim życiu?')
+
+  if msg.startswith('Osa w co grasz?'):
+    await message.channel.send('GTA ONLINE RP to jest PRAWDZIWE życie PRAWDZIWA policja!')
+
+  if msg.startswith('Osa jak tam?'):
+    await message.channel.send('Policja mnie goni!')
+
+  if msg.startswith('Osa pomocy'):
+    await message.channel.send('Zbiór komend')
+    await message.channel.send('Osa zdążysz?')
+    await message.channel.send('Osa jak tam giełda?')
+    await message.channel.send('Osa jesteś strasznie zimny')
+    await message.channel.send('Osa tak tak tak')
+    await message.channel.send('Osa ile kobiet wyruchałeś?')
+    await message.channel.send('Osa w co grasz?')
+    await message.channel.send('Osa jak tam?')
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
